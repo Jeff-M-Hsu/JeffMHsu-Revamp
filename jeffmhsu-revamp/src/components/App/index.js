c729583fde473d0e5a050f363c6b55e47dc2aa0c
Jeff-M-Hsu <jeff_m_hsu@hotmail.com>
2019-10-13 19:09:20 -0700

    Removed need to import images twice
    Passed image paths via props instead of importing them again in the Main component.

--- jeffmhsu-revamp/src/components/App/index.js
+++ jeffmhsu-revamp/src/components/App/index.js
@@ -11,107 +11,117 @@ import Bumblebee from '../../resources/img/bumblebee.png';
 import Jscrape from '../../resources/img/jscrape.png';
 import CC from '../../resources/img/currencyconverter.PNG';
 
-
 const App = () => {
   return (
   <Router>
     <React.Fragment>
       <TopBar />
-      <Switch>
-        <Route exact path='/' component={Main} />
-        <Route exact path='/jeffmhsu' component={
-          () => <Page
-            title="JeffMHsu.com"
-            img={Jeffmhsu}
-            date="July-August, 2019"
-            purpose="Personal Project"
-            stack="React | SASS | AWS S3/Route 53"
-            ext="https://github.com/Jeff-M-Hsu/JeffMHsu-Revamp"
-            description="JeffMHsu.com is my personal website that I designed and built from scratch
-              as a way to increase my online presence and have a personalized, visually 
-              comprehensive source to display my work. The design is modern, clean, professional, 
-              and has a responsive layout that transforms to support phones, tablets, 
-              as well as 2K and 4K displays on top of the typical laptop/desktop resolutions."
-          />} 
-        />
-        <Route exact path='/honeycombguild' component={
-          () => <Page
-            title="HoneyComb Guild"
-            img={Honeycomb}
-            date="June-July, 2019"
-            purpose="Commission"
-            stack="React | CSS (Bootstrap) | Express | Node.js | MongoDB | AWS S3/EC2"
-            ext="http://tinyurl.com/honeycombguild"
-            description="A gaming community named HoneyComb commissioned me to build a website for them to 
-              recruit new members with. It was designed in collaboration with the community leader and 
-              has a responsive layout that tranforms to support mobile, tablet, 2K/4K displays on top of 
-              the typical laptop/desktop resolutions. They also requested the ability to display bee jokes and facts on the website through an interactive medium. 
-              I fulfilled this request by creating a database of bee related information and an API that links to it. 
-              My choice to use react as the front end of the site was to give this feature the requested interactivity, although 
-              Angular would have been a great alternative."
-          />} 
-        />
-        <Route exact path='/fixmeadrink' component={
-          () => <Page
-            title="Fix Me A Drink"
-            img={Fixmeadrink}
-            date="August, 2019"
-            purpose="Personal Project"
-            stack="PHP | CSS | AWS EB/EC2"
-            ext="http://tinyurl.com/fixmeadrink"
-            description="A website that consumes TheCocktailDB API to output recipes of cocktails. The user can 
-              request a random cocktail or search for recipes by name or by ingredient. This website also 
-              has a responsive layout that supports mobile and 2K/4K resolutions. I built this website to get a feel for 
-              using PHP and practicing my design skills. It has also opened my eyes to some very strange cocktails, one of the recipes 
-              has several paragraphs detailing how to cook ribs in order to collect its fat for a mezcal based drink!"
-          />} 
-        />
-        <Route exact path='/bumblebee' component={
-          () => <Page
-            title="Bumblebee"
-            img={Bumblebee}
-            date="February-March, 2019"
-            purpose="Commission Sample"
-            stack="Node.js | Discord.io"
-            ext="https://github.com/Jeff-M-Hsu/Bumblebee"
-            description="A gaming community named HoneyComb asked me to create a Discord bot for their server to provide more 
-              personality to the server based on their theme. I created a bot that listens to keywords and sends corresponding 
-              messages back. The success of this project resulted in being commissioned to buid the HoneyComb website. 
-              I had to source all of the bee jokes, facts, pick up lines, etc. on my own, so I built a web scraper to aid in scouring 
-              pages for content and a separate program to unify the formatting of hundreds of messages. That web scraper is now its 
-              own product named JScrape."
-          />} 
-        />
-        <Route exact path='/jscrape' component={
-          () => <Page
-            title="JScrape"
-            img={Jscrape}
-            date="April, 2019"
-            purpose="Personal Project"
-            stack="Java | Swing | HtmlUnit"
-            ext="https://github.com/Jeff-M-Hsu/JScrape"
-            description="I built a web scraper in Java to save time on scouring through websites for bee jokes, facts, pick up lines, etc. while working on 
-              Bumblebee. I chose Java because at the time it was the language I was most proficient with. I chose HtmlUnit over JSoup because 
-              I felt that HtmlUnit would be a better tool to learn as it is typically used for testing web applications while being capable of being used 
-              for web scraping, which is more applicable to the type of development that I am pursuing. I also decided to make a GUI for it to make it easier 
-              to use and more accessible on short notice."
-          />} 
-        />
-        <Route exact path='/currencyconverter' component={
-          () => <Page
-            title="Currency Converter"
-            img={CC}
-            date="2017"
-            purpose="Personal Project"
-            stack="Java | Swing | HttpClient"
-            ext="https://github.com/Jeff-M-Hsu/Currency-Converter"
-            description="I created a desktop application to consume the currencylayer API. The currencylayer API holds records of both updated and historical 
-              currency exchange rates for over 168 currencies, including cryptocurrency such as Bitcoin. I wrote the program as a way of getting more 
-              practice with Java, Swing, and to learn about HttpClient, which provides a lightweight solution for sending requests to the API."
-          />} 
-        />
-      </Switch>
-    </React.Fragment>
+        <Switch>
+          <Route exact path='/' component={
+            () => <Main 
+              jeffmhsu={Jeffmhsu}
+              honeycomb={Honeycomb}
+              fixmeadrink={Fixmeadrink}
+              bumblebee={Bumblebee}
+              jscrape={Jscrape}
+              cc={CC}
+            />}
+          />
+          <Route exact path='/jeffmhsu' component={
+            () => <Page
+              title="JeffMHsu.com"
+              img={Jeffmhsu}
+              date="July-August, 2019"
+              purpose="Personal Project"
+              stack="React | SASS | AWS S3/Route 53"
+              ext="https://github.com/Jeff-M-Hsu/JeffMHsu-Revamp"
+              description="JeffMHsu.com is my personal website that I designed and built from scratch
+                as a way to increase my online presence and have a personalized, visually 
+                comprehensive source to display my work. The design is modern, clean, professional, 
+                and has a responsive layout that transforms to support phones, tablets, 
+                as well as 2K and 4K displays on top of the typical laptop/desktop resolutions. 
+                This website also qualifies as a Progressive Web App, and is capable of being viewed 
+                and navigated offline and as a standalone desktop/mobile app."
+            />} 
+          />
+          <Route exact path='/honeycombguild' component={
+            () => <Page
+              title="HoneyComb Guild"
+              img={Honeycomb}
+              date="June-July, 2019"
+              purpose="Commission"
+              stack="React | CSS (Bootstrap) | Express | Node.js | MongoDB | AWS S3/EC2"
+              ext="http://tinyurl.com/honeycombguild"
+              description="A gaming community named HoneyComb commissioned me to build a website for them to 
+                recruit new members with. It was designed in collaboration with the community leader and 
+                has a responsive layout that tranforms to support mobile, tablet, 2K/4K displays on top of 
+                the typical laptop/desktop resolutions. They also requested the ability to display bee jokes and facts on the website through an interactive medium. 
+                I fulfilled this request by creating a database of bee related information and an API that links to it. 
+                My choice to use react as the front end of the site was to give this feature the requested interactivity, although 
+                Angular would have been a great alternative."
+            />} 
+          />
+          <Route exact path='/fixmeadrink' component={
+            () => <Page
+              title="Fix Me A Drink"
+              img={Fixmeadrink}
+              date="August, 2019"
+              purpose="Personal Project"
+              stack="PHP | CSS | AWS EB/EC2"
+              ext="http://tinyurl.com/fixmeadrink"
+              description="A website that consumes TheCocktailDB API to output recipes of cocktails. The user can 
+                request a random cocktail or search for recipes by name or by ingredient. This website also 
+                has a responsive layout that supports mobile and 2K/4K resolutions. I built this website to get a feel for 
+                using PHP and practicing my design skills. It has also opened my eyes to some very strange cocktails, one of the recipes 
+                has several paragraphs detailing how to cook ribs in order to collect its fat for a mezcal based drink!"
+            />} 
+          />
+          <Route exact path='/bumblebee' component={
+            () => <Page
+              title="Bumblebee"
+              img={Bumblebee}
+              date="February-March, 2019"
+              purpose="Commission Sample"
+              stack="Node.js | Discord.io"
+              ext="https://github.com/Jeff-M-Hsu/Bumblebee"
+              description="A gaming community named HoneyComb asked me to create a Discord bot for their server to provide more 
+                personality to the server based on their theme. I created a bot that listens to keywords and sends corresponding 
+                messages back. The success of this project resulted in being commissioned to buid the HoneyComb website. 
+                I had to source all of the bee jokes, facts, pick up lines, etc. on my own, so I built a web scraper to aid in scouring 
+                pages for content and a separate program to unify the formatting of hundreds of messages. That web scraper is now its 
+                own product named JScrape."
+            />} 
+          />
+          <Route exact path='/jscrape' component={
+            () => <Page
+              title="JScrape"
+              img={Jscrape}
+              date="April, 2019"
+              purpose="Personal Project"
+              stack="Java | Swing | HtmlUnit"
+              ext="https://github.com/Jeff-M-Hsu/JScrape"
+              description="I built a web scraper in Java to save time on scouring through websites for bee jokes, facts, pick up lines, etc. while working on 
+                Bumblebee. I chose Java because at the time it was the language I was most proficient with. I chose HtmlUnit over JSoup because 
+                I felt that HtmlUnit would be a better tool to learn as it is typically used for testing web applications while being capable of being used 
+                for web scraping, which is more applicable to the type of development that I am pursuing. I also decided to make a GUI for it to make it easier 
+                to use and more accessible on short notice."
+            />} 
+          />
+          <Route exact path='/currencyconverter' component={
+            () => <Page
+              title="Currency Converter"
+              img={CC}
+              date="2017"
+              purpose="Personal Project"
+              stack="Java | Swing | HttpClient"
+              ext="https://github.com/Jeff-M-Hsu/Currency-Converter"
+              description="I created a desktop application to consume the currencylayer API. The currencylayer API holds records of both updated and historical 
+                currency exchange rates for over 168 currencies, including cryptocurrency such as Bitcoin. I wrote the program as a way of getting more 
+                practice with Java, Swing, and to learn about HttpClient, which provides a lightweight solution for sending requests to the API."
+            />} 
+          />
+        </Switch>
+      </React.Fragment>
     </Router>
   );
 }
--- jeffmhsu-revamp/src/components/Main/index.js
+++ jeffmhsu-revamp/src/components/Main/index.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 import '../../sass/main.scss';
 import Landing from './Landing';
 import Card from './Card';
-import Jeffmhsu from '../../resources/img/jeffmhsu.png';
-import Honeycomb from '../../resources/img/honeycomb.png';
-import Fixmeadrink from '../../resources/img/fixmeadrink.png';
-import Bumblebee from '../../resources/img/bumblebee.png';
-import Jscrape from '../../resources/img/jscrape.png';
-import CC from '../../resources/img/currencyconverter.PNG';
 
 class Main extends Component{
   render(){
@@ -15,12 +9,12 @@ class Main extends Component{
       <div className="main">
         <Landing />
         <div className="deck">
-          <Card image={ Jeffmhsu } page="/jeffmhsu" title="JeffMHsu [Revamped]" description="Personal website to showcase my work. You're on it right now! Now fully rewritten with React/SASS." tags="React | SASS | AWS S3/Route 53" />
-          <Card image={ Honeycomb } page="/honeycombguild" title="HoneyComb Guild" description="A website and accompanying API commissioned by a gaming community to recruit new members." tags="React | Express | Node.js | MongoDB | AWS S3/EC2" />
-          <Card image={ Fixmeadrink } page="fixmeadrink" title="Fix Me A Drink" description="A website that provides cocktail recipes. Users can request cocktail recipes by name, ingredient, or randomly." tags="PHP | CSS | AWS EB/EC2" />
-          <Card image={ Bumblebee } page="bumblebee" title="Bumblebee" description="A discord bot that talks about bees. Delivers bee jokes, facts, memes, and more." tags="Node.js | Discord.io | JScrape" />
-          <Card image={ Jscrape } page="jscrape" title="JScrape" description="An easy to use web scraper with a GUI." tags="Java | HtmlUnit | Java Swing" />
-          <Card image={ CC } page="currencyconverter" title="Currency Converter" description="Convert USD to up to 168 currencies including cryptocurrency in real time and historically." tags="Java | HttpClient | Java Swing" />
+          <Card image={ this.props.jeffmhsu } page="/jeffmhsu" title="JeffMHsu [Revamped]" description="Personal website to showcase my work. You're on it right now! Now fully rewritten with React/SASS." tags="React | SASS | AWS S3/Route 53" />
+          <Card image={ this.props.honeycomb } page="/honeycombguild" title="HoneyComb Guild" description="A website and accompanying API commissioned by a gaming community to recruit new members." tags="React | Express | Node.js | MongoDB | AWS S3/EC2" />
+          <Card image={ this.props.fixmeadrink } page="fixmeadrink" title="Fix Me A Drink" description="A website that provides cocktail recipes. Users can request cocktail recipes by name, ingredient, or randomly." tags="PHP | CSS | AWS EB/EC2" />
+          <Card image={ this.props.bumblebee } page="bumblebee" title="Bumblebee" description="A discord bot that talks about bees. Delivers bee jokes, facts, memes, and more." tags="Node.js | Discord.io | JScrape" />
+          <Card image={ this.props.jscrape } page="jscrape" title="JScrape" description="An easy to use web scraper with a GUI." tags="Java | HtmlUnit | Java Swing" />
+          <Card image={ this.props.cc } page="currencyconverter" title="Currency Converter" description="Convert USD to up to 168 currencies including cryptocurrency in real time and historically." tags="Java | HttpClient | Java Swing" />
         </div>
       </div>
     );
